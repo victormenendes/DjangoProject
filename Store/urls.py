ca5664681a54c848ee5a063b8d11e7cb0df32cfc
victormenendes <victormenendes3101@gmail.com>
2022-12-03 21:06:38 -0300

    FINALIZED HTML

--- Store/urls.py
+++ Store/urls.py
@@ -6,4 +6,8 @@ from . import views
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('produtos/', ProdutosView.as_view(), name='produtos'),
+    path('contato/', ContatoView.as_view(), name='contato'),
+    path('carrinho/', CarrinhoView.as_view(), name='carrinho'),
+    path('wishlist/', WishlistView.as_view(), name='wishlist'),
+    path('descricao-produto/', DescricaoProdutoView.as_view(), name='descricao-produto'),
 ]
--- Store/views.py
+++ Store/views.py
@@ -22,3 +22,35 @@ class ProdutosView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Produtos'
         return context
+
+class ContatoView(TemplateView):
+    template_name = 'Store/contato.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Contato'
+        return context
+
+class CarrinhoView(TemplateView):
+    template_name = 'Store/carrinho.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Carrinho'
+        return context
+
+class WishlistView(TemplateView):
+    template_name = 'Store/wishlist.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Wishlist'
+        return context
+
+class DescricaoProdutoView(TemplateView):
+    template_name = 'Store/descricao-produto.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Descricao-produto'
+        return context
